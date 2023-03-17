bdfc2de6d80cbc5a1a8240aab2a43966c36354a0
mahmood <zozo99999@icloud.com>
2023-03-17 21:50:04 +0300

    .

--- getSongLyric.py
+++ getSongLyric.py
@@ -5,15 +5,18 @@ import csv
 def songlyric(artist,song):
     songName = song.replace(" ","-")
     artistName = artist[0].upper() + artist[1:]
-    print(f"https://genius.com/{artistName}-{songName}-lyrics")
-    result = requests.get(f"https://genius.com/{artistName}-{songName}-lyrics")
+    artistFinalName = artistName.replace(" ","-")
+    print(f"https://genius.com/{artistFinalName}-{songName}-lyrics")
+    result = requests.get(f"https://genius.com/{artistFinalName}-{songName}-lyrics")
     soup = BeautifulSoup(result.text,"html.parser")
     main = soup.find("main",{"class","SongPage__Container-sc-19xhmoi-0 buKnHw"})
     if not main:
         print("sorry it look like you didnt write the song correct")
         return 
-    songInfo = main.find("div",{"class","SongPage__Section-sc-19xhmoi-3 cXvCRB"})
-    songlyrics = songInfo.find("div",{"class","Lyrics__Container-sc-1ynbvzw-6 YYrds"}).text.strip()
-    return songlyrics
+    else:
+        songInfo = main.find("div",{"class","SongPage__Section-sc-19xhmoi-3 cXvCRB"})
+        songlyrics = songInfo.find("div",{"class","Lyrics__Container-sc-1ynbvzw-6 YYrds"}).text.strip()
+        print(songlyrics)
+        return
 
-print(songlyric("hozier","take me to church"))
+songlyric("lana del rey","young and beautiful")
