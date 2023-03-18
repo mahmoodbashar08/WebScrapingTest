ff3072e71296f95c02cb79d2b001389a78b228b9
mahmood <zozo99999@icloud.com>
2023-03-18 18:37:44 +0300

    .

--- getSongLyric2.py
+++ getSongLyric2.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-def songLyric(query):
+def songLyric(songName):
     # Send a GET request to Google
+    if len(songName)>20 :
+        query = songName[:20]
+    else:
+        query = songName
     url = "https://www.google.com/search?q=كلمات+أغنية+" + query
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"}
     response = requests.get(url, headers=headers)
@@ -10,6 +14,7 @@ def songLyric(query):
     soup = BeautifulSoup(response.content, "html.parser")
     divs = soup.select_one("div#main")
     first_level = divs.select("div.BNeawe")
+    hello = ''
     for i in range(len(first_level)):
         if first_level[i].find_all("div",{"class","BNeawe tAd8D AP7Wnd"}):
             hello = first_level[i].find_all("div",{"class","BNeawe tAd8D AP7Wnd"})
@@ -19,6 +24,7 @@ def songLyric(query):
     if text !="":
         return text
     else:
+        text = 'i could not find the song'
         return text
 
-print(songLyric("hello adele"))
+print(songLyric("take me to church"))
